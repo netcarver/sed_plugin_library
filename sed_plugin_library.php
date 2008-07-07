51854834afd4deb70f739cd8cec4a55e98a96d9c
netcarver <nc.github@net-carving.com>
2008-07-08 01:20:54 +0800

    Remove old svn revision

--- sed_plugin_library.php
+++ sed_plugin_library.php
@@ -1,18 +1,7 @@
 <?php
 
-$plugin['revision'] = '$LastChangedRevision$';
-
-$revision = @$plugin['revision'];
-if( !empty( $revision ) )
-	{
-	$parts = explode( ' ' , trim( $revision , '$' ) );
-	$revision = $parts[1];
-	if( !empty( $revision ) )
-		$revision = '.' . $revision;
-	}
-
 $plugin['name'] = 'sed_plugin_library';
-$plugin['version'] = '0.4' . $revision;
+$plugin['version'] = '0.4';
 $plugin['author'] = 'Netcarver';
 $plugin['author_uri'] = 'http://txp-plugins.netcarving.com';
 $plugin['description'] = 'Helper functions for sed plugins.';
